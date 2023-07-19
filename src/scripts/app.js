d41780d8917dde4a7bce5034e29d942154f530e1
Eivo2023 <info@eivo.pl>
2023-07-19 17:09:30 +0200

    lotto

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,28 +1,89 @@
 const btnPlayRef = document.querySelector('#playBtn');
 const inputRefs = [...document.querySelectorAll('input[id^="digit"]')];
+const resultsRef = document.querySelector('.result');
 
-//deklaruję funkcję i przekazuję do niej parametry a później te dane wyjmujemy po wywołaniu funkcji
-const isAllNotEmpty = (nodes) => nodes.every((node) => node.value.trim() !== ''); // (nodes) o tylko nazwa zmiennej
-const isItegers = (nodes) => nodes.every((node) => Number.isInteger(Number(node.value)) ); //deklaracja funkcji pod nazwą const 'nazwa'
-const convertToIntegers = (nodes) => nodes.map((node) => parseInt(node.value)); // funkcja która zwraca intigery
+
+const isAllNotEmpty = (nodes) => nodes.every((node) => node.value.trim() !== '');
+const isIntegers = (nodes) => nodes.every((node) => Number.isInteger(Number(node.value)));
+const convertToIntegers = (nodes) => nodes.map((node) => parseInt(node.value));
 const isInRange = (digits) => digits.every((digit) => digit >= 1 && digit <= 49);
-// => digits.every((digit) => digit >= 1 && digit <= 49); - to co jest po strzałce wywołuje funkcję
-const isNotRedundant = (digits) => new Set(digits).size === digits.lenght; //tworzy set, który usuwa duplikaty, size to rozmiar seta (tablicy w tym przypadku)
-btnPlayRef.addEventListener('click', function(event) {
-    // console.log(event.target);
-    // console.log(this);
-    if (isAllNotEmpty(inputRefs)) {
-        if (isItegers(inputRefs)) {
-            const userDigits = convertToIntegers(inputRefs);
-            // console.log('ok')
-            if (isInRange(userDigits)) { //userDigits wpada do zmiennej digits
-                if (isNotRedundant(userDigits)) {
+const isNotRedundant = (digits) => new Set(digits).size === digits.length;
 
+const drawDigits = () => {
+    const numbers = [];
+
+    while (numbers.length < 6) {
+        const result = Math.round(Math.random() * 48 + 1);
+        if (!numbers.includes(result)) {
+            numbers.push(result);
+        }
+    }
+
+    return numbers;
+}
+
+const checkHits = (userDigits, drawnDigits) => {
+    const hits = [];
+
+    // userDigits.forEach((digit) => {
+    //     if (drawnDigits.includes(digit)) {
+    //         hits.push(digit);
+    //     }
+    // })
+
+    for (const digit of userDigits) {
+        if (drawnDigits.includes(digit)) {
+            hits.push(digit);
+        }
+    }
+
+    return hits;
+}
+
+const checkHitsPro = (userDigits, drawnDigits) => userDigits.filter((digit) => drawnDigits.includes(digit));
+
+const calculatePrize = (quantity) => {
+    switch (quantity) {
+        case 3:
+            return 24;
+        case 4:
+            return 170;
+        case 5:
+            return 3500;
+        case 6:
+            return 3000000;
+        default:
+            return 0
+    }
+};
+
+const showResults = (hits, drawnDigits) => {
+    let message = `Wylosowane liczby to: ${drawnDigits.join(', ')}. `;
+    if (hits.length > 0) {
+        message += `Trafiłeś ${hits.length} razy, twoje liczby to: ${hits.join(', ')}.`
+        message += `Hajs: ${calculatePrize(hits.length)}PLN.`
+    } else {
+        message += 'Nic nie wygrałeś, spróbuj jeszcze raz, a na pewno wygrasz!'
+    }
+
+    resultsRef.innerText = message;
+}
+
+
+btnPlayRef.addEventListener('click', function (event) {
+    if (isAllNotEmpty(inputRefs)) {
+        if (isIntegers(inputRefs)) {
+            const userDigits = convertToIntegers(inputRefs);
+            if (isInRange(userDigits)) {
+                if (isNotRedundant(userDigits)) {
+                    const drawnDigits = drawDigits();
+                    const hits = checkHits(userDigits, drawnDigits);
+                    showResults(hits, drawnDigits);
                 } else {
-                    console.log('liczby się powtarzają ziom')
+                    console.log('Liczby ci się powtarzają ziomeczku')
                 }
             } else {
-                console.log('liczby są po za zakresem 1-49')
+                console.log('są liczby poza zakresem 1-49')
             }
         } else {
             console.log('liczby nie są liczbami')
@@ -30,5 +91,30 @@ btnPlayRef.addEventListener('click', function(event) {
     } else {
         console.log('coś jest puste')
     }
-
 })
+
+
+function becomeMillionaire(money, digits) {
+    const games = money / 3;
+    let prize = 0;
+    const count6 = [];
+
+    for (let i = 0; i < games; i++) {
+        let userDigits;
+        if (digits === undefined) {
+            userDigits = drawDigits();
+        } else {
+            userDigits = digits;
+        }
+
+        const drawnDigits = drawDigits();
+        const hits = checkHits(userDigits, drawnDigits);
+        prize += calculatePrize(hits.length)
+
+        if (hits.length === 6) {
+            count6.push(hits);
+        }
+    }
+
+    return `Wygrałeś ${prize}PLN, szóstki: ${count6.length}, trafione numery do szóstki: ${count6.join(', ')}`
+}
